b1bce92847f036fc655fe9312873cd409e53f8f0
PedroEvaldt <pedrofossati067@gmail.com>
2026-04-26 15:41:36 -0300

    build: add codex files and AGENTS.md to .gitignore

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,15 @@
 package config
+
+import (
+	"github.com/PedroEvaldt/recall/internal/storage/database"
+)
+
+type Config struct {
+	db *db.Queries
+}
+
+func GetConfig(db *db.Queries) *Config {
+	return &Config{
+		db: db,
+	}
+}
